18caf7b5b7d5a0e291cc01b64e297c7edf5ee445
Clément Cardi <google-oauth2|100735160333204498652>
2021-04-23 09:25:00 +0000

    update

--- includes/constants.js
+++ includes/constants.js
@@ -1,2 +1,18 @@
 const PROJECT_ID = "pod-fr-retail";
-module.exports = { PROJECT_ID };
+const date_start="2017-09-01"
+
+function regionGroup(city) {
+  return `CASE
+          WHEN ${city} IN ("US", "CA") THEN "NA"
+          WHEN ${city} IN ("GB", "FR", "DE", "IT", "PL") THEN "EU"
+          WHEN ${city} IN ("AU") THEN ${city}
+          ELSE "Other regions"
+          END`;
+};
+
+function returnLastDate(table_name){
+  return `SELECT max(DATE(tdate)) last_date,
+  FROM ${table_name}}`
+}
+
+module.exports = { PROJECT_ID, date_start, returnLastDate, regionGroup };
